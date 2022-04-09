40715ba7afcdc5bd5e2426ae34295a9f46faf7c9
zddyjke <didar.erdaulet@mail.ru>
2022-04-09 09:42:31 +0600

    nar comit

---
+++ src/main/java/kz/narxoz/narxozapp/model/User.java
@@ -0,0 +1,29 @@
+package kz.narxoz.narxozapp.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+}
